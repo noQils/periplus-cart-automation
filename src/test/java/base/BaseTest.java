ef0b0607dbec5fa9160379630276f8fb3b40f6ed
noQils <daffaaqilmahmud@gmail.com>
2026-06-06 15:11:28 +0700

    test: add shared Selenium setup and teardown

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,34 @@
+package base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+    protected final String baseUrl = "https://www.periplus.com/";
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get(baseUrl);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
